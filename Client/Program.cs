77d4dc5e9723d2d4dd4c140dc86490d1c9fb9e98
TomasNieto19 <tnieto852@gmail.com>
2023-08-29 19:08:17 -0300

    Agrego cliente en .NET

---
+++ Client/Program.cs
@@ -0,0 +1,16 @@
+﻿﻿using Grpc.Net.Client;
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+
+        using var channel = GrpcChannel.ForAddress("http://localhost:8081");
+
+        var client = new Greeter.GreeterClient(channel);
+
+        var reply = await client.SayHelloAsync(new HelloRequest { Name = "World" });
+
+        Console.WriteLine("Greeting from .NET: " + reply.Message);
+    }
+}
