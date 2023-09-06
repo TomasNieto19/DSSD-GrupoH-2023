e41605afaa28d0e06f4531306c734092a6124db3
TomasNieto19 <tnieto852@gmail.com>
2023-09-06 17:27:00 -0300

    Add get followers and follow/unfollow Client

--- Client/Controllers/UserController.cs
+++ Client/Controllers/UserController.cs
@@ -22,11 +22,31 @@ namespace Client.Controllers
 
         }
 
+        [HttpGet("followers/{id}")]
+        public async Task<IActionResult> GetFollowers(int id)
+        {
+
+            var request = new getFollowersRequest { IdUser = id };
+
+            return Ok(await userServiceClient.getFollowersAsync(request));
+
+        }
+
         [HttpPost("addUser")]
         public async Task<IActionResult> AddUser(UserDto userDto)
         {
 
             return Ok(await userServiceClient.addUserAsync(userDto));
+
+        }
+
+        [HttpPost("followers/{idFollower}/{idFollowing}")]
+        public async Task<IActionResult> FollowAction(int idFollower, int idFollowing)
+        {
+
+            var request = new followActionRequest { IdFollower = idFollower, IdFollowing = idFollowing };
+
+            return Ok(await userServiceClient.followActionAsync(request));
 
         }
 
