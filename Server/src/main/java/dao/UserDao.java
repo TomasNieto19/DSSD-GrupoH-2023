4119d3c3d56474b2cf52c2e5b20e228d09e8c993
TomasNieto19 <tnieto852@gmail.com>
2023-09-06 18:44:15 -0300

    Add get followers and follow/unfollow Server

--- Server/src/main/java/dao/UserDao.java
+++ Server/src/main/java/dao/UserDao.java
@@ -2,17 +2,14 @@ package dao;
 
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
+import org.hibernate.Hibernate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 import entities.User;
 
 public class UserDao {
-	/*
-	 * METODOS A IMPLEMENTAR: 
-	 * - LISTADO DE USUARIOS QUE SIGUE. 
-	 * - LISTADO DE RECETAS FAVORITAS.
-	 */
-
+		
 	private static UserDao instance;
 
 	// Patron Singleton para reutilizar la instancia en el serivce
@@ -26,15 +23,16 @@ public class UserDao {
 
 	
 	// Metodo para persistir un usuario en la BD
-	public void addUser(User user) throws Exception {
+	public User addUser(User user) throws Exception {
 
 		EntityManager em = JPAUtil.getEMF().createEntityManager();
-	
 
+		User userAdd = null;
+		
 		try {
 
 			em.getTransaction().begin();
-			em.persist(user);
+			userAdd = em.merge(user);
 			em.getTransaction().commit();
 
 		} catch (Exception e) {
@@ -42,9 +40,11 @@ public class UserDao {
 			throw new Exception("Error al persistir Usuario: " + e.getMessage());
 
 		} finally {
-			
+
 			em.close();
 		}
+		
+		return userAdd;
 	}
 
 	
@@ -83,10 +83,30 @@ public class UserDao {
 		} finally {
 
 			em.close();
-
 		}
 
 		return user;
+	}
+
+	
+	// Metodo que retorla la lista de usrios que sigue el userId
+	public Set<User> getUserFollowers(int userId) {
+
+		EntityManager em = JPAUtil.getEMF().createEntityManager();
+		User user = null;
+
+		try {
+
+			user = em.find(User.class, userId);
+
+			Hibernate.initialize(user.getFollowers());
+
+		} finally {
+
+			em.close();
+		}
+
+		return user.getFollowers();
 	}
 
 	
--- Server/src/main/java/entities/User.java
+++ Server/src/main/java/entities/User.java
@@ -1,30 +1,34 @@
 package entities;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import java.util.Set;
+import java.util.HashSet;
 
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
-@ToString
+@ToString(exclude = "followers")
+@EqualsAndHashCode(exclude = "followers")
 @Setter
 @Getter
 @Table(name = "users")
 public class User {
-	/*
-	 * ATRIBUTOS QUE FALTAN: 
-	 * List<Usuario> usuarios que sigue 
-	 * List<Receta> recetas favoritas
-	 */
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -42,6 +46,10 @@ public class User {
 
 	@Column(name = "password", nullable = false)
 	private String password;
+
+	@ManyToMany(fetch = FetchType.LAZY, cascade = { CascadeType.ALL })
+	@JoinTable(name = "followers", joinColumns = @JoinColumn(name = "id_follower"), inverseJoinColumns = @JoinColumn(name = "id_following"))
+	private Set<User> followers = new HashSet<User>();
 
 	public User(String name, String email, String username, String password) {
 		this.name = name;
