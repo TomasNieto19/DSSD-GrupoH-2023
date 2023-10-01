9babf1b60b0a74fdbd61c51c787a2245c5aadef6
Matías Ferreira <55554362+Mctias00@users.noreply.github.com>
2023-10-01 20:09:39 -0300

    punto1

--- Kafka/consumer.js
+++ Kafka/consumer.js
@@ -3,39 +3,33 @@ import { KafkaConfig } from "./KafkaConfig.js";
 const kafka = new KafkaConfig();
 
 export const getKafkaMessages = async (req, res) => {
-  
   const consumer = kafka.consumer;
 
   try {
-
-    // 1 - Conexion con el servidor de Kafka
+    // 1 - Conexión con el servidor de Kafka
     await consumer.connect();
-    
-    // 2 - Se suscripcion al topico de Kafka, desde el principio
-    await consumer.subscribe({topic: "my-topic", fromBeginning: true})
 
-    // 3 - Se consumen los mensajes del topico
+    // 2 - Suscripción al tópico de Kafka, desde el principio
+    await consumer.subscribe({ topic: "newnovedades", fromBeginning: true });
+
+    // 3 - Consumir los mensajes del tópico
     consumer.run({
       eachBatchAutoResolve: false,
       eachBatch: async ({ batch }) => {
-
-        let messsages = []
+        let messages = [];
 
         for (let message of batch.messages) {
-
-          messsages.push(JSON.parse(message.value.toString()))
-
+          messages.push(JSON.parse(message.value.toString()));
         }
 
-        res.json(messsages)
-
-        consumer.disconnect()
-      }
-    })
-    
+        // Responder al cliente con los mensajes
+        res.json(messages);
+      },
+    });
   } catch (error) {
-
     console.error("ERROR EN CONSUMER: " + error);
-
+  } finally {
+    // 4 - Desconexión del consumidor (debe estar fuera del bucle)
+    await consumer.disconnect();
   }
 };
--- Kafka/producer.js
+++ Kafka/producer.js
@@ -3,31 +3,34 @@ import { KafkaConfig } from "./KafkaConfig.js";
 const kafka = new KafkaConfig();
 
 export const produceMessageKafka = async (req, res) => {
-
   const producer = kafka.producer;
-  
+
   try {
-
-    // 1 - Conexion con el servidor de Kafka
+    // 1 - Conexión con el servidor de Kafka
     await producer.connect();
-    
-    // 2 - Se convierte el body a JSON String
-    const message = JSON.stringify(req.body);
-   
-    // 3 - Envia el mensaje al topico de Kafka
-    await producer.send({topic: "my-topic", messages:[{value: message}]})
-   
-    // 4 - Responde al cliente
-    res.status(200).json({ message: "Mensaje recibido!" });
 
+    // 2 - Extraer información necesaria del cuerpo de la solicitud (req.body)
+    const { username, recipeTitle, firstPhotoUrl } = req.body;
+
+    // 3 - Crear un objeto con la información de la receta
+    const recipeInfo = {
+      username,
+      recipeTitle,
+      firstPhotoUrl,
+    };
+
+    // Convertir el objeto a JSON String
+    const message = JSON.stringify(recipeInfo);
+
+    // 4 - Enviar el mensaje al tópico de Kafka "Novedades"
+    await producer.send({ topic: "newnovedades", messages: [{ value: message }] });
+
+    // 5 - Responder al cliente
+    res.status(200).json({ message: "La receta se ha publicado con éxito!" });
   } catch (error) {
-
     console.error("ERROR EN PRODUCER: " + error);
-
   } finally {
-    
-    // 5 - Se desconecta del servidor Kafka
+    // 6 - Desconexión del servidor Kafka
     await producer.disconnect();
-
   }
 };
