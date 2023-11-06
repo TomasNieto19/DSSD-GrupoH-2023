5cf8eb3ef6bd2ddab6fd5dcc273547381be3ce4d
Matías Ferreira <55554362+Mctias00@users.noreply.github.com>
2023-11-05 23:21:22 -0300

    addService

--- Soap-Server/src/main/java/services/ISeleccionDelChefService.java
+++ Soap-Server/src/main/java/services/ISeleccionDelChefService.java
@@ -8,10 +8,11 @@ import javax.jws.WebResult;
 import javax.jws.WebService;
 import javax.jws.soap.SOAPBinding;
 import javax.jws.soap.SOAPBinding.Use;
+import javax.persistence.EntityManager;
 
+import dao.JPAUtil;
 import entities.RecetaEnSeleccionDelChef;
-import entities.RecipeBook;
-import entities.Recipe_in_RecipeBook;
+import entities.RecetasChef;
 import entities.SeleccionDelChef;
 
 @WebService
@@ -36,7 +37,7 @@ SeleccionDelChef getSeleccionDelChefById( @WebParam(name = "id") int idSelecChef
 String deleteSeleccionDelChef(@WebParam(name = "idRecipeBook") int idSelecChef);
 
  
-
+//otra clase
 
 
 @WebMethod(operationName = "addRecetaEnSeleccionDelChef") //nombre de la operacion
@@ -55,5 +56,72 @@ RecetaEnSeleccionDelChef getRecetaEnSeleccionDelChefById( @WebParam(name = "id")
 @WebMethod(operationName = "deleteRecetaEnSeleccionDelChef") //nombre de la operacion
 @WebResult(name = "ResultadoOperacion") //nombre de donde nos va a dejar el resultado
 String deleteRecetaEnSeleccionDelChef(@WebParam(name = "idRecetario") int idRecetario);
+
+
+//otra clase
+
+
+ 
+
+@WebMethod(operationName = "addRecetasChef") //nombre de la operacion
+@WebResult(name = "ResultadoOperacion") //nombre de donde nos va a dejar el resultado
+String addRecetasChef(@WebParam(name = "titulo") String title, @WebParam(name = "descripcion") String description,
+		@WebParam(name = "tiempoCoccion") int cookTime,@WebParam(name = "ingredientes")  String ingredients,
+		@WebParam(name = "pasos") String steps,@WebParam(name = "id_user") Integer users,
+		@WebParam(name = "foto") String photoUrl);
+
+
+@WebMethod(operationName = "getAllRecetasChef") //nombre de la operacion
+@WebResult(name = "ResultadoOperacion") //nombre de donde nos va a dejar el resultado
+List<RecetasChef> getAllRecetasChef();//me devuelve una lista de recetarios
+
+@WebMethod(operationName = "getRecetasChefByUserId") //nombre de la operacion
+@WebResult(name = "ResultadoOperacion") //nombre de donde nos va a dejar el resultado
+RecetasChef getRecetasChefByUserId( @WebParam(name = "id_user") int users);//devuelve un recetario
+
+@WebMethod(operationName = "deleteAllRecetasChef") //nombre de la operacion
+@WebResult(name = "ResultadoOperacion") //nombre de donde nos va a dejar el resultado
+String deleteAllRecetasChef(@WebParam(name = "recetas_chef") String tabla);
+
+@WebMethod(operationName = "getRecetaById") //nombre de la operacion solo id de la receta$$$$$$$
+@WebResult(name = "ResultadoOperacion") //nombre de donde nos va a dejar el resultado
+RecetasChef getRecetaById( @WebParam(name = "id") int idRecipe);//devuelve un recetario
+
+@WebMethod(operationName = "deleteRecetaChef") //nombre de la operacion
+@WebResult(name = "ResultadoOperacion") //nombre de donde nos va a dejar el resultado
+String deleteRecetaChef(@WebParam(name = "idRecipe") int idRecipe);
+
+
+//Recetario Chef Dao
+
+@WebMethod(operationName = "addOrUpdateSeleccionDelChef")
+@WebResult(name = "ResultadoOperacion")
+
+public static SeleccionDelChef addOrUpdateSeleccionDelChef(@WebParam(name = "seleccionDelChef") SeleccionDelChef seleccionDelChef) throws Exception {
+	EntityManager em = JPAUtil.getEMF().createEntityManager();
+
+	SeleccionDelChef SeleccionDelChefAdd = null;
+
+	try {
+		em.getTransaction().begin();
+		SeleccionDelChefAdd = em.merge(seleccionDelChef);
+		em.getTransaction().commit();
+
+	} catch (Exception e) {
+
+		throw new Exception("Error al persistir SeleccionDelChef: ");
+
+	} finally {
+
+		em.close();
+	}
+
+	return SeleccionDelChefAdd;
+
+
+}
+@WebMethod(operationName = "getAll") //nombre de la operacion
+@WebResult(name = "ResultadoOperacion") //nombre de donde nos va a dejar el resultado
+List<SeleccionDelChef> getAll();//me devuelve una lista de recetarios
 
 }
